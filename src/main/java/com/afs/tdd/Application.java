98cfcf71e25b014f5357c0354422d99134b57f78
NSC/Network Service Center <NSC@oocl.com>
2020-11-26 17:00:57 +0800

    [MarsRover][Dominic Leung] test: implement test for y plus 1 given N and M

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,4 +1,38 @@
 package com.afs.tdd;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+    private int locationX;
+    private int locationY;
+    private String direction;
+
+    public Application(int locationX, int locationY, String direction) {
+
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+    public void roverControl(String commands){
+        splitInructions(commands).forEach(this::excuteIntruction);
+    }
+
+    private List<String> splitInructions(String commands) {
+        return Arrays.asList(commands.split(""));
+    }
+    private void excuteIntruction(String command){
+
+    }
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
 }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,19 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    public void should_return_y_plus_1_when_command_is_M_given_0_0_N(){
+        //given
+        Application marsRover=new Application(0,0,"N");
+        String commands ="M";
+        //when
+        marsRover.roverControl(commands);
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(1,marsRover.getLocationY());
+        assertEquals("N",marsRover.getDirection());
     }
 }
