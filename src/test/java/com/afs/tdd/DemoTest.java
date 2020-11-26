5a363ecb537c857ba8be00a14c502f5faaeff8a6
NSC/Network Service Center <NSC@oocl.com>
2020-11-26 17:39:43 +0800

    [MarsRover][Dominic Leung] test: implement for direction N given W and R

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -89,4 +89,16 @@ class DemoTest {
         assertEquals(0,marsRover.getLocationY());
         assertEquals("W",marsRover.getDirection());
     }
+    @Test
+    public void should_return_direction_N_when_command_is_R_given_0_0_W(){
+        //given
+        Application marsRover=new Application(0,0,"W");
+        String commands ="R";
+        //when
+        marsRover.roverControl(commands);
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("N",marsRover.getDirection());
+    }
 }
